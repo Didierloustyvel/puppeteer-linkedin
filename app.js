94bc870eafd8feb84cffb15a8c78a3a9792fae27
lou <loustyveldidier88@gmail.com>
2021-11-01 15:05:35 +0100

    make it better

--- app.js
+++ app.js
@@ -13,22 +13,24 @@ var start_tor = function(){
 async function start_browser() {
     await start_tor();
     browser = await puppeteer.launch({
-        headless: true,
+        headless: false,
         timeout : 0,
         defaultViewport: null,
-        slowMo : 0,
+        slowMo : 1000,
+        // args [ '--proxy-server=127.0.0.1:9876' ],
         args: ['--no-sandbox', '--start-maximized', '--disable-setuid-sandbox', '--disable-infobars', '--window-position=0,0', '--ignore-certifcate-errors', '--ignore-certifcate-errors-spki-list',]
     })
     const page = await browser.newPage();
+    await page.goto("http://www.mon-ip.com/", { timeout: 0, waitUntil: 'load' });
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36');
-
     await page.goto("https://www.linkedin.com", { timeout: 0, waitUntil: 'domcontentloaded' });
-    // await page.evaluate(() => document.cookie = "li_at=AQEDAS3yEQQEPWgxAAABfNXfzDsAAAF8-exQO00Ahojcycm-bRT89RE2hF070b7jR6XF-L1rbUF8Zi6liqumY8OCVlg0qDyiv-kxSWDHj9IVnZsjPdhOl86fzyEmCBi0-_VWO71FsXKi4p6tLQWC-Hpm" );
-    // await page.goto("https://www.linkedin.com", { timeout: 0, waitUntil: 'domcontentloaded' });
-    await page.goto("http://www.mon-ip.com/", { timeout: 0, waitUntil: 'domcontentloaded' });
-    await page.goto("https://www.linkedin.com", { timeout: 0, waitUntil: 'domcontentloaded' });
-    await page.goto("https://www.whatismybrowser.com/fr/detect/what-is-my-user-agent", { timeout: 0, waitUntil: 'domcontentloaded' });
-    console.log('ok')
+    await page.evaluate(() => document.cookie = "li_at=AQEDAS3yEQQCEMRtAAABfNujCVQAAAF8_6-NVE0AH0RHbAxPwKp6mbOyMOf3vXk6Phs5qeRDW2PRlAiwpVRtkBOR4bw7x27Ss9sCuwrArg7oHEmNElICOqSde94LQh0WFnIC4sUaN-_Sb19a82QhOKpu" );
+    await page.goto("https://www.linkedin.com", { timeout: 0, waitUntil: 'networkidle2' });
+    await page.goto("https://www.linkedin.com/mynetwork/", { timeout: 0, waitUntil: 'networkidle2' });
+    await page.waitForSelector('[class="artdeco-button artdeco-button--2 artdeco-button--secondary ember-view full-width"]')
+    await page.evaluate(() => {
+        Array.from(document.querySelectorAll('[class="artdeco-button artdeco-button--2 artdeco-button--secondary ember-view full-width"]'), e => e.click())
+    });
 }
 
 
