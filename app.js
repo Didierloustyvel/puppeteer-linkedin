f8c4b78fc4fa37dd5763e6295885fea5cabec805
lou <loustyveldidier88@gmail.com>
2021-11-01 15:06:12 +0100

    make it better

--- app.js
+++ app.js
@@ -24,13 +24,14 @@ async function start_browser() {
     await page.goto("http://www.mon-ip.com/", { timeout: 0, waitUntil: 'load' });
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36');
     await page.goto("https://www.linkedin.com", { timeout: 0, waitUntil: 'domcontentloaded' });
-    await page.evaluate(() => document.cookie = "li_at=AQEDAS3yEQQCEMRtAAABfNujCVQAAAF8_6-NVE0AH0RHbAxPwKp6mbOyMOf3vXk6Phs5qeRDW2PRlAiwpVRtkBOR4bw7x27Ss9sCuwrArg7oHEmNElICOqSde94LQh0WFnIC4sUaN-_Sb19a82QhOKpu" );
+    await page.evaluate(() => document.cookie = "li_at=" );
     await page.goto("https://www.linkedin.com", { timeout: 0, waitUntil: 'networkidle2' });
     await page.goto("https://www.linkedin.com/mynetwork/", { timeout: 0, waitUntil: 'networkidle2' });
     await page.waitForSelector('[class="artdeco-button artdeco-button--2 artdeco-button--secondary ember-view full-width"]')
     await page.evaluate(() => {
         Array.from(document.querySelectorAll('[class="artdeco-button artdeco-button--2 artdeco-button--secondary ember-view full-width"]'), e => e.click())
     });
+
 }
 
 
